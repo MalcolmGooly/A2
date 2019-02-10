dd863c9954516954f4b1df89ab7615a72a0a0db4
Malcolm Goolamallee <gool0005@algonquinlive.com>
2019-02-10 17:33:21 -0500

    Test5

--- js/app.js
+++ js/app.js
@@ -1,49 +1,29 @@
+/*
+    To start this app:
+    1. run the command 'node app.js' within the 
+    app folder (probably called 'WebVR_Introduction').
+    2. Visit localhost:8080/
+*/
+
+//app setup
 const express   = require('express');
 const app       = express();
 const http      = require('http');
 const server    = http.createServer(app);
-const socketIO  = require('socket.io')(server); //hello I am new
 
-const LISTEN_PORT = 8080; //make sure greater than 3000. Some ports are reserved/blocked by firewall ...
+//const vars
+const LISTEN_PORT = 8080;
 
-app.use((express.static(__dirname + '/public'))); //set root dir to the public folder
+//middleware
+app.use(express.static(__dirname + '/public'));
 
-//routes
-app.get('/color', function(req,res) {
-    res.sendFile(__dirname + '/public/color.html');
+//set routes
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + 'public/index.html');
 });
 
-app.get('/controller', function(req,res) {
-    res.sendFile(__dirname + '/public/controller.html');
-});
+//!!TRY: create another route to point to another web page
 
-//websocket stuff
-socketIO.on('connection', function(socket) {
-    console.log(socket.id + ' has connected!');
-
-    socket.on('disconnect', function(data) {
-        console.log(socket.id + ' has disconnected');
-    });
-
-    //custom events
-    //socket = one client
-    //socketIO.sockets = all clients
-    socket.on('red', function(data) {
-        console.log('red event heard');
-        socketIO.sockets.emit('color_change', {r:255, g:0, b:0});
-    });
-
-    socket.on('green', function(data) {
-        console.log('green event heard');
-        socketIO.sockets.emit('color_change', {r:0, g:255, b:0});
-    });
-
-    socket.on('blue', function(data) {
-        console.log('blue event heard');
-        socketIO.sockets.emit('color_change', {r:0, g:0, b:255});
-    });
-});
-
-//finally, start server
+//start server
 server.listen(LISTEN_PORT);
-console.log('listening to port: 8080' + LISTEN_PORT);
+console.log('Listening on port: 8080' + LISTEN_PORT );
